9f72b1d83b8e0aefab2119ffcbfa5f86b56a2cdc
Hamish Robertson <hnorobertson@gmail.com>
2016-02-03 00:38:47 +0000

    Fix for 1.0.5. Tidy up code. Set assembly version to match HoneyFox's mod versioning.

--- TweakableGimbal.cs
+++ TweakableGimbal.cs
@@ -18,16 +18,15 @@ namespace TweakableGimbal
 		private List<Quaternion> m_initRots = null;
 
 		// Test utilities.
-
 		[KSPField(isPersistant = false, guiActive = false, guiActiveEditor = true, guiName = "Yaw Test"),
-		UI_FloatRange(minValue = -1.0f, maxValue = 1.0f, scene = UI_Scene.Editor, stepIncrement = 0.1f)]
-		public float yawTest = 0.0f;
+		UI_FloatRange(minValue = 0f, maxValue = 2f, scene = UI_Scene.Editor, stepIncrement = 0.01f)]
+		public float yawTest = 1f;
 		[KSPField(isPersistant = false, guiActive = false, guiActiveEditor = true, guiName = "Pitch Test"),
-		UI_FloatRange(minValue = -1.0f, maxValue = 1.0f, scene = UI_Scene.Editor, stepIncrement = 0.1f)]
-		public float pitchTest = 0.0f;
+		UI_FloatRange(minValue = 0f, maxValue = 2f, scene = UI_Scene.Editor, stepIncrement = 0.01f)]
+		public float pitchTest = 1f;
 		[KSPField(isPersistant = false, guiActive = false, guiActiveEditor = true, guiName = "Roll Test"),
-		UI_FloatRange(minValue = -1.0f, maxValue = 1.0f, scene = UI_Scene.Editor, stepIncrement = 0.1f)]
-		public float rollTest = 0.0f;
+		UI_FloatRange(minValue = 0f, maxValue = 2f, scene = UI_Scene.Editor, stepIncrement = 0.01f)]
+		public float rollTest = 1f;
 
 		// Tweakable.
 
@@ -86,22 +85,55 @@ namespace TweakableGimbal
 
 		public override void OnStart(PartModule.StartState state)
 		{
-			m_startState = state;
-
-			BindGimbal();
-
-			if (m_startState == StartState.Editor)
+			try
 			{
-				if (m_gimbal != null)
+				m_startState = state;
+
+				BindGimbal();
+
+				((Fields["m_maxGimbalRange"].uiControlEditor) as UI_FloatRange).maxValue = m_gimbal.gimbalRange;
+				((Fields["m_maxGimbalRange"].uiControlFlight) as UI_FloatRange).maxValue = m_gimbal.gimbalRange;
+
+				m_maxGimbalRange = m_gimbal.gimbalRange;
+
+				if (state == StartState.Editor)
 				{
-					//Debug.Log("Getting initRots.");
-					FieldInfo fi = typeof(ModuleGimbal).GetField("initRots", BindingFlags.NonPublic | BindingFlags.Instance);
-					m_initRots = (List<Quaternion>)fi.GetValue(m_gimbal);
+					if (m_gimbal != null)
+					{
+						//Debug.Log("Getting initRots.");
+						m_initRots = m_gimbal.initRots;
+					}
+
+					//Debug.Log("TweakableGimbal: OnStart");
 				}
-				
-				this.InvokeRepeating("OnUpdate", 0.1f, 0.1f);
-				//Debug.Log("TweakableGimbal: OnStart");
+
+				if (state != StartState.Editor)
+				{
+					if (yawCoeffH == 0.0)
+						Fields["yawReverseH"].guiActive = false;
+
+					if (yawCoeffV == 0.0)
+						Fields["yawReverseV"].guiActive = false;
+
+					if (pitchCoeffH == 0.0)
+						Fields["pitchReverseH"].guiActive = false;
+
+					if (pitchCoeffV == 0.0)
+						Fields["pitchReverseV"].guiActive = false;
+
+					if (rollCoeffH == 0.0)
+						Fields["rollReverseH"].guiActive = false;
+
+					if (rollCoeffV == 0.0)
+						Fields["rollReverseV"].guiActive = false;
+				}
 			}
+			catch (Exception e)
+			{
+				Debug.LogError("OnStart: " + e.Message);
+				throw e;
+			}
+
 		}
 
 		public void BindGimbal()
@@ -117,140 +149,126 @@ namespace TweakableGimbal
 			}
 		}
 
-		public override void OnUpdate()
+		private Vector3 TransformGimbal(Vector3 ctrlVecVessel, float ctrlRoll)
 		{
-			((Fields["m_maxGimbalRange"].uiControlEditor) as UI_FloatRange).maxValue = m_gimbal.gimbalRange;
-			((Fields["m_maxGimbalRange"].uiControlFlight) as UI_FloatRange).maxValue = m_gimbal.gimbalRange;
-			
-			//Debug.Log("TweakableGimbal: OnUpdate");
-			if (m_startState != StartState.Editor)
-			{
-				if (yawCoeffH == 0.0)
-					Fields["yawReverseH"].guiActive = false;
-				else
-					Fields["yawReverseH"].guiActive = true;
-				if (yawCoeffV == 0.0)
-					Fields["yawReverseV"].guiActive = false;
-				else
-					Fields["yawReverseV"].guiActive = true;
-				if (pitchCoeffH == 0.0)
-					Fields["pitchReverseH"].guiActive = false;
-				else
-					Fields["pitchReverseH"].guiActive = true;
-				if (pitchCoeffV == 0.0)
-					Fields["pitchReverseV"].guiActive = false;
-				else
-					Fields["pitchReverseV"].guiActive = true;
-				if (rollCoeffH == 0.0)
-					Fields["rollReverseH"].guiActive = false;
-				else
-					Fields["rollReverseH"].guiActive = true;
-				if (rollCoeffV == 0.0)
-					Fields["rollReverseV"].guiActive = false;
-				else
-					Fields["rollReverseV"].guiActive = true;
-				return;
-			}
-
-			float ctrlYaw = 0.0f;
-			float ctrlPitch = 0.0f;
-			float ctrlRoll = 0.0f;
-
-			Vector3 vesselRefVec = new Vector3(yawTest, 0.0f, -pitchTest);
-			Vector3 ctrlVecVessel = EditorLogic.startPod.transform.TransformDirection(vesselRefVec);
 			Vector3 ctrlVecLocal = this.transform.InverseTransformDirection(ctrlVecVessel);
-			//Debug.Log("ctrlVecVessel: " + ctrlVecVessel.ToString() + "  ctrlVecLocal: " + ctrlVecLocal.ToString());
 
-			ctrlYaw = ctrlVecLocal.x;
-			ctrlPitch = ctrlVecLocal.z;
-			ctrlRoll = -rollTest;
+			float ctrlYaw = ctrlVecLocal.x;
+			float ctrlPitch = ctrlVecLocal.z;
 
-			if (m_gimbal != null)
-			{
+			float yawValueH = ctrlYaw * m_maxGimbalRange * yawCoeffH * (yawReverseH ? -1.0f : 1.0f);
+			float yawValueV = ctrlYaw * m_maxGimbalRange * yawCoeffV * (yawReverseV ? -1.0f : 1.0f);
+			float pitchValueH = ctrlPitch * m_maxGimbalRange * pitchCoeffH * (pitchReverseH ? -1.0f : 1.0f);
+			float pitchValueV = ctrlPitch * m_maxGimbalRange * pitchCoeffV * (pitchReverseV ? -1.0f : 1.0f);
+			float rollValueH = ctrlRoll * m_maxGimbalRange * rollCoeffH * (rollReverseH ? -1.0f : 1.0f);
+			float rollValueV = ctrlRoll * m_maxGimbalRange * rollCoeffV * (rollReverseV ? -1.0f : 1.0f);
+
+			Vector3 ctrlOutVecLocal = new Vector3(
+				Mathf.Clamp(yawValueH + pitchValueH + rollValueH, -m_maxGimbalRange, m_maxGimbalRange),
+				0.0f,
+				Mathf.Clamp(yawValueV + pitchValueV + rollValueV, -m_maxGimbalRange, m_maxGimbalRange)
+			);
+			Vector3 ctrlOutVecVessel = this.transform.TransformDirection(ctrlOutVecLocal);
+
+			return ctrlOutVecVessel;
+		}
+
+		public void Update()
+		{
+			try {
+				if (m_gimbal == null)
+				{
+					Debug.Log("Update: No gimbal");
+					return;
+				}
+				if (m_startState != StartState.Editor)
+				{
+					return;
+				}
+
 				if (m_gimbal.gimbalLock == false)
 				{
-					float yawValueH = ctrlYaw * m_maxGimbalRange * yawCoeffH * (yawReverseH ? -1.0f : 1.0f);
-					float yawValueV = ctrlYaw * m_maxGimbalRange * yawCoeffV * (yawReverseV ? -1.0f : 1.0f);
-					float pitchValueH = ctrlPitch * m_maxGimbalRange * pitchCoeffH * (pitchReverseH ? -1.0f : 1.0f);
-					float pitchValueV = ctrlPitch * m_maxGimbalRange * pitchCoeffV * (pitchReverseV ? -1.0f : 1.0f);
-					float rollValueH = ctrlRoll * m_maxGimbalRange * rollCoeffH * (rollReverseH ? -1.0f : 1.0f);
-					float rollValueV = ctrlRoll * m_maxGimbalRange * rollCoeffV * (rollReverseV ? -1.0f : 1.0f);
+					Vector3 vesselRefVec = new Vector3(yawTest - 1f, 0.0f, -(pitchTest - 1f));
+					Vector3 ctrlVecVessel = EditorLogic.RootPart.transform.TransformDirection(vesselRefVec);
 
-					Vector3 ctrlOutVecLocal = new Vector3(Mathf.Clamp(yawValueH + pitchValueH + rollValueH, -m_maxGimbalRange, m_maxGimbalRange), 0.0f, Mathf.Clamp(yawValueV + pitchValueV + rollValueV, -m_maxGimbalRange, m_maxGimbalRange));
-					Vector3 ctrlOutVecVessel = this.transform.TransformDirection(ctrlOutVecLocal);
-					Vector3 ctrlOutVecRef = EditorLogic.startPod.transform.InverseTransformDirection(ctrlOutVecVessel);
-					//Debug.Log("ctrlOutVecLocal: " + ctrlOutVecLocal.ToString() + "  ctrlOutVecVessel: " + ctrlOutVecVessel.ToString() + "  ctrlOutVecRef: " + ctrlOutVecRef.ToString());
+					Vector3 ctrlOutVecVessel = this.TransformGimbal(ctrlVecVessel, -(rollTest - 1f));
 
-					m_gimbal.gimbalAngleH = Mathf.Clamp(ctrlOutVecRef.x, -m_maxGimbalRange, m_maxGimbalRange);
-					m_gimbal.gimbalAngleV = Mathf.Clamp(-ctrlOutVecRef.z, -m_maxGimbalRange, m_maxGimbalRange);
+					Vector3 ctrlOutVecRef = EditorLogic.RootPart.transform.InverseTransformDirection(ctrlOutVecVessel);
+
+					m_gimbal.gimbalAngleYaw = Mathf.Clamp(ctrlOutVecRef.x, -m_maxGimbalRange, m_maxGimbalRange);
+					m_gimbal.gimbalAnglePitch = Mathf.Clamp(-ctrlOutVecRef.z, -m_maxGimbalRange, m_maxGimbalRange);
 				}
-				//
-				//Debug.Log("Try to rotate the nozzle");
 
-				// Use the gimbal's OnFixedUpdate.
-				if (EditorLogic.startPod != null)
+				if (EditorLogic.RootPart != null)
+				{
 					for (int i = 0; i < m_gimbal.gimbalTransforms.Count; ++i)
 					{
 						Quaternion q = m_initRots[i];
 						Transform transform = m_gimbal.gimbalTransforms[i];
 
-						Vector3 axisH = transform.InverseTransformDirection(EditorLogic.startPod.transform.forward);
-						Vector3 axisV = transform.InverseTransformDirection(EditorLogic.startPod.transform.right);
-						
-						Quaternion qH = Quaternion.AngleAxis(m_gimbal.gimbalAngleH, axisH);
-						Quaternion qV = Quaternion.AngleAxis(m_gimbal.gimbalAngleV, axisV);
+						Vector3 axisH = transform.InverseTransformDirection(EditorLogic.RootPart.transform.forward);
+						Vector3 axisV = transform.InverseTransformDirection(EditorLogic.RootPart.transform.right);
+
+						Quaternion qH = Quaternion.AngleAxis(m_gimbal.gimbalAngleYaw, axisH);
+						Quaternion qV = Quaternion.AngleAxis(m_gimbal.gimbalAnglePitch, axisV);
 						transform.localRotation = q * qV * qH;
 					}
+				}
 			}
-			else
+			catch (NullReferenceException e)
 			{
-				//Debug.Log("Cannot find gimbal.");
+				Debug.LogError("OnUpdate: " + e.StackTrace);
+			}
+			catch (Exception e)
+			{
+				Debug.LogError("OnUpdate: " + e.Message);
+				throw e;
 			}
 		}
 
 		public override void OnFixedUpdate()
 		{
-			if (m_startState == StartState.Editor) return;
-
-			float ctrlYaw = 0.0f;
-			float ctrlPitch = 0.0f;
-			float ctrlRoll = 0.0f;
-
-			Vector3 vesselRefVec = new Vector3(vessel.ctrlState.yaw, 0.0f, -vessel.ctrlState.pitch);
-			Vector3 ctrlVecVessel = vessel.ReferenceTransform.TransformDirection(vesselRefVec);
-			Vector3 ctrlVecLocal = this.transform.InverseTransformDirection(ctrlVecVessel);
-			
-			ctrlYaw = ctrlVecLocal.x;
-			ctrlPitch = ctrlVecLocal.z;
-			ctrlRoll = -vessel.ctrlState.roll; 
-			
-			if (m_gimbal.gimbalLock == false)
+			try
 			{
-				float yawValueH = ctrlYaw * m_maxGimbalRange * yawCoeffH * (yawReverseH ? -1.0f : 1.0f);
-				float yawValueV = ctrlYaw * m_maxGimbalRange * yawCoeffV * (yawReverseV ? -1.0f : 1.0f);
-				float pitchValueH = ctrlPitch * m_maxGimbalRange * pitchCoeffH * (pitchReverseH ? -1.0f : 1.0f);
-				float pitchValueV = ctrlPitch * m_maxGimbalRange * pitchCoeffV * (pitchReverseV ? -1.0f : 1.0f);
-				float rollValueH = ctrlRoll * m_maxGimbalRange * rollCoeffH * (rollReverseH ? -1.0f : 1.0f);
-				float rollValueV = ctrlRoll * m_maxGimbalRange * rollCoeffV * (rollReverseV ? -1.0f : 1.0f);
+				if (m_gimbal == null)
+				{
+					Debug.Log("OnFixedUpdate: No gimbal");
+					return;
+				}
 
-				Vector3 ctrlOutVecLocal = new Vector3(Mathf.Clamp(yawValueH + pitchValueH + rollValueH, -m_maxGimbalRange, m_maxGimbalRange), 0.0f, Mathf.Clamp(yawValueV + pitchValueV + rollValueV, -m_maxGimbalRange, m_maxGimbalRange));
-				Vector3 ctrlOutVecVessel = this.transform.TransformDirection(ctrlOutVecLocal);
-				Vector3 ctrlOutVecRef = vessel.ReferenceTransform.InverseTransformDirection(ctrlOutVecVessel);
+				if (m_gimbal.gimbalLock == false)
+				{
+					Vector3 vesselRefVec = new Vector3(vessel.ctrlState.yaw, 0.0f, -vessel.ctrlState.pitch);
+					Vector3 ctrlVecVessel = vessel.ReferenceTransform.TransformDirection(vesselRefVec);
+
+					Vector3 ctrlOutVecVessel = this.TransformGimbal(ctrlVecVessel, -vessel.ctrlState.roll);
 				
-				if (m_useGimbalResponseSpeed)
-				{
-					float deltaTime = TimeWarp.deltaTime;
+					Vector3 ctrlOutVecRef = vessel.ReferenceTransform.InverseTransformDirection(ctrlOutVecVessel);
 
-					float newGimbalH = Mathf.Lerp(m_gimbal.gimbalAngleH, Mathf.Clamp(ctrlOutVecRef.x, -m_maxGimbalRange, m_maxGimbalRange), m_gimbalResponseSpeed * deltaTime);
-					m_gimbal.gimbalAngleH = newGimbalH;
-					float newGimbalV = Mathf.Lerp(m_gimbal.gimbalAngleV, Mathf.Clamp(-ctrlOutVecRef.z, -m_maxGimbalRange, m_maxGimbalRange), m_gimbalResponseSpeed * deltaTime);
-					m_gimbal.gimbalAngleV = newGimbalV;
+					if (m_useGimbalResponseSpeed)
+					{
+						float deltaTime = TimeWarp.deltaTime;
+
+						float newGimbalH = Mathf.Lerp(m_gimbal.gimbalAngleYaw, Mathf.Clamp(ctrlOutVecRef.x, -m_maxGimbalRange, m_maxGimbalRange), m_gimbalResponseSpeed * deltaTime);
+						m_gimbal.gimbalAngleYaw = newGimbalH;
+						float newGimbalV = Mathf.Lerp(m_gimbal.gimbalAnglePitch, Mathf.Clamp(-ctrlOutVecRef.z, -m_maxGimbalRange, m_maxGimbalRange), m_gimbalResponseSpeed * deltaTime);
+						m_gimbal.gimbalAnglePitch = newGimbalV;
+					}
+					else
+					{
+						m_gimbal.gimbalAngleYaw = Mathf.Clamp(ctrlOutVecRef.x, -m_maxGimbalRange, m_maxGimbalRange);
+						m_gimbal.gimbalAnglePitch = Mathf.Clamp(-ctrlOutVecRef.z, -m_maxGimbalRange, m_maxGimbalRange);
+					}
 				}
-				else
-				{
-					m_gimbal.gimbalAngleH = Mathf.Clamp(ctrlOutVecRef.x, -m_maxGimbalRange, m_maxGimbalRange);
-					m_gimbal.gimbalAngleV = Mathf.Clamp(-ctrlOutVecRef.z, -m_maxGimbalRange, m_maxGimbalRange);
-				}
+			}
+			catch (NullReferenceException e)
+			{
+				Debug.LogError("OnFixedUpdate: " + e.StackTrace);
+			}
+			catch (Exception e)
+			{
+				Debug.LogError("OnFixedUpdate: " + e.Message);
+				throw e;
 			}
 		}
 
